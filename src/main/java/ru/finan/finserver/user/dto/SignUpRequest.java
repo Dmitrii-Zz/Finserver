7f5b97964d5dd5214f7f44bf3389bc6db647ac7d
Dmitrii-Z <zolotuhindimitrii@yandex.ru>
2024-03-06 22:05:39 +0700

    refactor: Документация swagger

--- src/main/java/ru/finan/finserver/user/dto/SignUpRequest.java
+++ src/main/java/ru/finan/finserver/user/dto/SignUpRequest.java
@@ -1,19 +1,28 @@
 package ru.finan.finserver.user.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
+@Schema(description = "Запрос на регистрацию")
 public class SignUpRequest {
 
-    @NotBlank
+    @Schema(description = "Имя пользователя", example = "Дмитрий")
+    @NotBlank(message = "Имя пользователя не может быть пустым")
+    @Size(max = 50, message = "Имя пользователя должно содержать не более 50 символов")
     private String name;
 
-    @Email
-    @NotBlank
+    @Schema(description = "Адрес ЭП", example = "Dimon@mail.ru")
+    @Email(message = "Email адрес должен быть в формате example@example.ru")
+    @Size(max = 255, message = "Email адрес не должен быть более 255 символов")
+    @NotBlank(message = "Email адрес не может быть пустым")
     private String email;
 
-    @NotBlank
+    @Schema(description = "Пароль", example = "ghjuhfvvbcnjn<juf")
+    @NotBlank(message = "Пароль не может быть пустым")
+    @Size(min = 8, max = 255, message = "Длина пароля должна быть от 8 до 255 символов")
     private String password;
 }
