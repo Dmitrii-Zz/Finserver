270d4f58831eb8bea82180f9367769ae1c50d591
Dmitrii-Z <zolotuhindimitrii@yandex.ru>
2024-03-22 00:40:31 +0700

    feat: написать метод создания траты

--- src/main/java/ru/finan/finserver/finanse/controller/FinanceController.java
+++ src/main/java/ru/finan/finserver/finanse/controller/FinanceController.java
@@ -1,18 +1,27 @@
 package ru.finan.finserver.finanse.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import ru.finan.finserver.finanse.dto.ExpenseDto;
+import ru.finan.finserver.finanse.service.FinanceService;
 
-@Service
+@Slf4j
+@RestController
 @RequiredArgsConstructor
-@RequestMapping("/finance")
+@RequestMapping("/finserver")
 public class FinanceController {
+    private final FinanceService financeService;
 
-    public String addSpending(@RequestBody @Validated ExpenseDto expenseDto) {
-        return null;
+    @PostMapping
+    public ExpenseDto addSpending(Authentication authentication,
+                                  @RequestBody @Validated ExpenseDto expenseDto) {
+        log.info("Запрос на создание траты");
+        return financeService.addExpense(authentication, expenseDto);
     }
 }
