a660b2a02571d65d9eb48b7f2f74ff648a6aea8c
Dmitrii-Z <zolotuhindimitrii@yandex.ru>
2024-03-22 00:42:25 +0700

    feat: реализация метода создания траты

--- src/main/java/ru/finan/finserver/finanse/service/FinanceService.java
+++ src/main/java/ru/finan/finserver/finanse/service/FinanceService.java
@@ -1,5 +1,24 @@
 package ru.finan.finserver.finanse.service;
 
-public class FinanceService
-{
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+import ru.finan.finserver.category.model.Category;
+import ru.finan.finserver.category.service.CategoryService;
+import ru.finan.finserver.finanse.dto.ExpenseDto;
+import ru.finan.finserver.finanse.repository.ExpenseRepository;
+import ru.finan.finserver.trend.service.TrendService;
+
+@Service
+@RequiredArgsConstructor
+public class FinanceService {
+    private final ExpenseRepository expenseStorage;
+    private final CategoryService categoryService;
+    private final TrendService trendService;
+
+    public ExpenseDto addExpense(Authentication authentication, ExpenseDto expenseDto) {
+        Category category = categoryService.findCategoryByName(expenseDto.getCategory());
+
+        return null;
+    }
 }
