b2fea91e9ea527a9d639c2de219c80d29f3ff1f1
Dmitrii-Z <zolotuhindimitrii@yandex.ru>
2024-03-24 00:23:44 +0700

    feat: отлов исключения ненайденного тренда

--- src/main/java/ru/finan/finserver/exceptions/controller/ErrorHandle.java
+++ src/main/java/ru/finan/finserver/exceptions/controller/ErrorHandle.java
@@ -5,17 +5,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import ru.finan.finserver.exceptions.model.CategoryByNameNotFoundException;
-import ru.finan.finserver.exceptions.model.ErrorResponse;
-import ru.finan.finserver.exceptions.model.UserNameNotFoundException;
-import ru.finan.finserver.exceptions.model.UserNotFoundException;
+import ru.finan.finserver.exceptions.model.*;
 
 @Slf4j
 @RestControllerAdvice
 public class ErrorHandle {
 
     @ExceptionHandler({UserNotFoundException.class, UserNameNotFoundException.class,
-                       CategoryByNameNotFoundException.class})
+                       CategoryByNameNotFoundException.class, TrendByNameNotFoundException.class})
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handleUserNotFoundException (final Exception e) {
         log.debug("Произошла ошибка {}", e.getMessage());
