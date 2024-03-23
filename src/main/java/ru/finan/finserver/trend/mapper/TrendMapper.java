f6ef5f567ebc5ab4b747f3c774a824f5b884f118
Dmitrii-Z <zolotuhindimitrii@yandex.ru>
2024-03-24 00:29:22 +0700

    feat: написать маппер для тренад

--- src/main/java/ru/finan/finserver/trend/mapper/TrendMapper.java
+++ src/main/java/ru/finan/finserver/trend/mapper/TrendMapper.java
@@ -1,4 +1,19 @@
 package ru.finan.finserver.trend.mapper;
 
+import ru.finan.finserver.trend.dto.TrendDto;
+import ru.finan.finserver.trend.model.Trend;
+
 public class TrendMapper {
+
+    public static Trend toTrend(TrendDto trendDto) {
+        return Trend.builder()
+                .name(trendDto.getName())
+                .build();
+    }
+
+    public static TrendDto toTrendDto(Trend trend) {
+        return TrendDto.builder()
+                .name(trend.getName())
+                .build();
+    }
 }
