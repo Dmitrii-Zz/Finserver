df1a75567c013e12773c5e6eb2d5f4dcf66f2cd1
Dmitrii-Z <zolotuhindimitrii@yandex.ru>
2024-03-24 00:27:12 +0700

    fix: изменить название полей траты дто

--- src/main/java/ru/finan/finserver/finanse/dto/ExpenseDto.java
+++ src/main/java/ru/finan/finserver/finanse/dto/ExpenseDto.java
@@ -5,8 +5,9 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Data;
+import ru.finan.finserver.category.dto.CategoryByExpenseDto;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 @Data
 @Builder
@@ -14,15 +15,15 @@ import java.time.LocalDateTime;
 public class ExpenseDto {
 
     @Schema(description = "Дата создания формируется на сервере", example = "09.03.2024")
-    private LocalDateTime created;
+    private LocalDate created;
 
     @NotNull
     @Schema(description = "Категория траты", example = "Продукты")
-    private String category;
+    private CategoryByExpenseDto nameCategory;
 
     @NotNull
     @Schema(description = "Направление траты", example = "Семья")
-    private String trend;
+    private String nameTrend;
 
     @Size(max = 255, message = "Заметка должна содержать не более 255 символов.")
     @Schema(description = "Краткая заметка", example = "Хлеб")
