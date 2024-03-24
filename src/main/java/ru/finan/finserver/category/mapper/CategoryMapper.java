98d1e448e4b590085ceaf9c5fe446e69f2b608cf
Dmitrii-Z <zolotuhindimitrii@yandex.ru>
2024-03-24 19:13:38 +0700

    feat: написать маппер для ДТО категории

--- src/main/java/ru/finan/finserver/category/mapper/CategoryMapper.java
+++ src/main/java/ru/finan/finserver/category/mapper/CategoryMapper.java
@@ -1,6 +1,7 @@
 package ru.finan.finserver.category.mapper;
 
 import org.springframework.stereotype.Component;
+import ru.finan.finserver.category.dto.CategoryByExpenseDto;
 import ru.finan.finserver.category.dto.CategoryDto;
 import ru.finan.finserver.category.model.Category;
 
@@ -14,6 +15,20 @@ public class CategoryMapper {
     }
     public static CategoryDto toCategoryDto(Category category) {
         return CategoryDto.builder()
+                .name(category.getName())
+                .isSpending(category.getIsSpending())
+                .build();
+    }
+
+    public static Category toCategory(CategoryByExpenseDto categoryByExpenseDto) {
+        return Category.builder()
+                .name(categoryByExpenseDto.getName())
+                .isSpending(categoryByExpenseDto.getIsSpending())
+                .build();
+    }
+
+    public static CategoryByExpenseDto toCategoryByExpenseDto(Category category) {
+        return CategoryByExpenseDto.builder()
                 .name(category.getName())
                 .isSpending(category.getIsSpending())
                 .build();
