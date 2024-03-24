a746a635c66f5a33e98f61352b34ab604e07ca4b
Dmitrii-Z <zolotuhindimitrii@yandex.ru>
2024-03-24 19:14:56 +0700

    feat: изменить запросный метод для БД категорий

--- src/main/java/ru/finan/finserver/category/repository/CategoryRepository.java
+++ src/main/java/ru/finan/finserver/category/repository/CategoryRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ru.finan.finserver.category.model.Category;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-    Category findByNameAndUserId(String name, long userId);
+    Category findByNameAndUserIdAndIsSpending(String name, long userId, boolean isSpending);
 }
