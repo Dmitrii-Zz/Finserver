cf386a1fafd907e42eb0c0b8edc0e73ea3dbcbef
Dmitrii-Z <zolotuhindimitrii@yandex.ru>
2024-03-24 19:12:58 +0700

    feat: написать ДТО категории

--- src/main/java/ru/finan/finserver/category/dto/CategoryByExpenseDto.java
+++ src/main/java/ru/finan/finserver/category/dto/CategoryByExpenseDto.java
@@ -1,4 +1,23 @@
 package ru.finan.finserver.category.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "DTO категории для объекта траты")
 public class CategoryByExpenseDto {
+
+    @NotBlank
+    @Schema(description = "Имя категории", example = "Коммуналка")
+    private String name;
+
+    @NotNull
+    @Schema(description = "Является ли категория тратой", example = "true")
+    private Boolean isSpending;
 }
