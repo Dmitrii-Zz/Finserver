42d62956ab718c3c74c7fc273902b884b0fd0f11
Dmitrii-Z <zolotuhindimitrii@yandex.ru>
2024-03-24 19:28:50 +0700

    feat: написать имя метода создания траты

--- src/main/java/ru/finan/finserver/finanse/mapper/ExpenseMapper.java
+++ src/main/java/ru/finan/finserver/finanse/mapper/ExpenseMapper.java
@@ -1,5 +1,6 @@
 package ru.finan.finserver.finanse.mapper;
 
+import ru.finan.finserver.category.mapper.CategoryMapper;
 import ru.finan.finserver.finanse.dto.ExpenseDto;
 import ru.finan.finserver.finanse.model.Expense;
 
@@ -7,6 +8,7 @@ public class ExpenseMapper {
 
     public static Expense toExpense(ExpenseDto expenseDto) {
         return Expense.builder()
+                .category(CategoryMapper.toCategory(expenseDto.getCategory()))
                 .note(expenseDto.getNote())
                 .quantity(expenseDto.getQuantity())
                 .created(expenseDto.getCreated())
@@ -16,7 +18,7 @@ public class ExpenseMapper {
     public static ExpenseDto toExpenseDto(Expense expense) {
         return ExpenseDto.builder()
                 .created(expense.getCreated())
-                .nameCategory(expense.getCategory().getName())
+                .category(CategoryMapper.toCategoryByExpenseDto(expense.getCategory()))
                 .nameTrend(expense.getTrend().getName())
                 .note(expense.getNote())
                 .quantity(expense.getQuantity())
